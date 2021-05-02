4ac9c5156f75eb919f18c510937c2c0c91bbf591
Nathan Wilcox <nathan-at-least@users.noreply.github.com>
2021-05-02 12:56:57 -0700

    Source the nix profile into the installation proc.

--- nixinst.sh
+++ nixinst.sh
@@ -7,4 +7,5 @@ set -efuxo pipefail
 curl -L https://nixos.org/nix/install > ./nixos.org-install.sh
 sh -- ./nixos.org-install.sh --no-daemon
 rm ./nixos.org-install.sh
+source ~/.nix-profile/etc/profile.d/nix.sh
 nix-env -i tmux vim git
