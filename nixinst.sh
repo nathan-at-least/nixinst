3f52c2c87b8ea706b9f7e1a5e22187f4c425ab03
Nathan Wilcox <nathan-at-least@users.noreply.github.com>
2021-05-02 12:09:54 -0700

    Require bash; fix a installer arg parsing bug.

--- nixinst.sh
+++ nixinst.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Usage: `curl 'https://raw.githubusercontent.com/nathan-at-least/nixinst/main/nixinst.sh' | sh`
+# Usage: `curl 'https://raw.githubusercontent.com/nathan-at-least/nixinst/main/nixinst.sh' | bash`
 
 set -efuxo pipefail
 
-curl -L https://nixos.org/nix/install | sh --no-daemon
+curl -L https://nixos.org/nix/install | sh -- --no-daemon
 nix-env -i tmux vim git
